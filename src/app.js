221f7bf1fc7d9f14e00bb643149307a8b0b86b09
SennCode <andres.alonso.senn@gmail.com>
2022-12-12 11:57:34 +0000

    domainGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// let dominio = [".com", ".org", ".net", ".io"];
+// let pronoun = ["the", "our"];
+// let adj = ["great", "big"];
+// let noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// for (let i = 0; i < pronoun.length; i++) {
+//   for (let j = 0; j < noun.length; j++) {
+//     for (let p = 0; p < adj.length; p++) {
+//       for (let r = 0; r < dominio.length; r++) {
+//         console.log(`${pronoun[i]}${noun[j]}${adj[p]}${dominio[r]}`);
+//       }
+//     }
+//   }
+// }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+
+// let combination = pronoun
+//   .concat(adj)
+//   .concat(noun)
+//   .forEach(function(item) {
+//     console.log(item);
+//   });
+pronoun.forEach(element => {
+  return adj.forEach(elemento2 => {
+    console.log(element + elemento2);
+  });
+});
